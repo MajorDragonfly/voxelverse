393d71eb0faef14de7a553eec80a917c5c102b0c
MajorDragonfly <lars_vdg@gmx.de>
2026-09-06 19:29:21 +0200

    Drive terrain material from planetary palette and altitude

---
+++ world/visuals/terrain/terrain_chunk_visuals_v8.gd
@@ -0,0 +1,31 @@
+extends "res://world/visuals/terrain/terrain_chunk_visuals_v7_runtime.gd"
+
+
+func _apply_terrain_material(terrain_mesh: MeshInstance3D) -> void:
+	super._apply_terrain_material(terrain_mesh)
+	var material := terrain_mesh.material_override as ShaderMaterial
+	if material == null:
+		return
+	var profile: Dictionary = WorldGenerator.get_planet_profile()
+	var palette: Dictionary = profile.get("palette", {})
+	var rock: Color = palette.get("rock", Color(0.37, 0.38, 0.39, 1.0))
+	var snow: Color = palette.get("snow", Color(0.86, 0.90, 0.91, 1.0))
+	var grass: Color = palette.get("grass", Color(0.30, 0.54, 0.28, 1.0))
+	material.set_shader_parameter(&"rock_color", Vector3(rock.r, rock.g, rock.b))
+	var strata: Color = rock.lightened(0.12).lerp(Color(0.48, 0.31, 0.20, 1.0), 0.30)
+	material.set_shader_parameter(&"rock_strata_color", Vector3(strata.r, strata.g, strata.b))
+	material.set_shader_parameter(&"snow_color", Vector3(snow.r, snow.g, snow.b))
+	var wet: Color = grass.darkened(0.44).lerp(Color(0.06, 0.18, 0.15, 1.0), 0.45)
+	material.set_shader_parameter(&"wet_ground_color", Vector3(wet.r, wet.g, wet.b))
+	material.set_shader_parameter(
+		&"snow_start_altitude",
+		float(profile.get("snow_start_altitude", 18.0))
+	)
+	material.set_shader_parameter(
+		&"snow_end_altitude",
+		float(profile.get("snow_end_altitude", 23.0))
+	)
+	material.set_shader_parameter(&"rocky_height", 6.0)
+	material.set_shader_parameter(&"rock_slope_start", 0.22)
+	material.set_shader_parameter(&"rock_slope_end", 0.58)
+	material.set_shader_parameter(&"strata_strength", 0.14)
